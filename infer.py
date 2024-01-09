db6e8f548fb11875cb371eb308add1200810323d
MAV-r <80701036+MAV-r@users.noreply.github.com>
2024-01-09 23:54:35 +0300

    added train infer code

---
+++ infer.py
@@ -0,0 +1,25 @@
+import torch
+import pandas as pd
+
+from model import FraudNet
+
+def main():
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    model = FraudNet().to(device)
+    model.load_state_dict(torch.load('./models/'))
+    model.eval()
+
+    X_test = pd.read_parquet('./data/X_test.parquet')
+    y_test = pd.read_parquet('./data/y_test.parquet')
+
+    with torch.no_grad():
+        logits = model(X_test)
+
+    # y_test_true = [int(x) for x in y_test.cpu().detach().numpy()]
+    y_pred = [1 if x >= 0.5 else 0 for x in logits.cpu().detach().numpy()]
+
+    pd.DataFrame(y_pred).to_csv('./data/predictions.csv')
+
+if __name__ == "__main__":
+    main()
---
+++ train.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import torch
+
+from anomaly_detection.model import FraudNet, train
+from torch.utils.data import TensorDataset, DataLoader
+from anomaly_detection.dataset import datasets_preparation
+
+def main():
+
+    datasets_preparation()
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    net = FraudNet().to(device)
+
+    X_train = pd.read_parquet('./data/X_train.parquet')
+    y_train = pd.read_parquet('./data/y_train.parquet')
+    X_val = pd.read_parquet('./data/X_val.parquet')
+    y_val = pd.read_parquet('./data/y_val.parquet')
+
+
+    train_dataset = TensorDataset(torch.tensor(X_train, dtype=torch.float32),
+                                  torch.tensor(np.array(y_train), dtype=torch.float32))
+    val_dataset = TensorDataset(torch.tensor(X_val, dtype=torch.float32),
+                                torch.tensor(np.array(y_val), dtype=torch.float32))
+
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=2, pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False, num_workers=2, pin_memory=True)
+
+    optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
+
+    train(net, optimizer, 1, train_loader, val_loader) #, scheduler=scheduler
+
+    torch.save(net.state_dict(), "./models/")
+
+if __name__ =='__main__':
+    main()
